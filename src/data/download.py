d39525a5c95c693aefb71c98b773d70089cc4e94
Keesiu Wong <keesiu.wong@gmail.com>
2018-09-04 23:14:25 +0200

    create download.py

---
+++ src/data/download.py
@@ -0,0 +1,45 @@
+import requests, zipfile, io, os, logging, argparse
+import pandas as pd
+
+logger = logging.getLogger()
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s %(levelname)s %(message)s')
+def main(args):
+  github_list = pd.read_csv(args.repo_list_path)
+  github_list.index = github_list.index.map(str)
+  logger.info('Loaded repo list with {} entries'.format(len(github_list)))
+
+  # drop all entries, that don't have a repo entry
+  github_list = github_list.dropna(subset=['GithubRepoLink'])
+  logger.info('There are {} entries with a repo link'.format(len(github_list)))
+
+  for _, row in github_list.iterrows():
+    # check if the folder already exists
+    final_download_path = os.path.join(args.output_path, str(row['Id']))
+    if os.path.exists(final_download_path):
+      logger.info('Repo {} already exists'.format(str(row['Id'])))
+      continue
+
+    repo_url = row['GithubRepoLink']
+    repo_zip_url = repo_url + '/archive/master.zip'
+    try:
+      logger.info('Downloading repo of Team {} with id {!s}'.format(row['TeamName'], str(row['Id'])))
+      req = requests.get(repo_zip_url, allow_redirects=True)
+      req.raise_for_status()
+      z = zipfile.ZipFile(io.BytesIO(req.content))
+      z.extractall(final_download_path)
+      logger.info('Sucessfully extracted {} repo'.format(row['TeamName']))
+    except (requests.exceptions.RequestException, requests.exceptions.HTTPError) as e:
+      logger.error(e)
+     
+
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser(description='Download and unpack Github repos from a csv file')
+  parser.add_argument('repo_list_path', help='path to the list of github repos')
+  parser.add_argument('output_path', help='path where the downloaded repos will be stored')
+  args = parser.parse_args()
+  main(args)
+
+
+
+

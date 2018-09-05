5df8b49f79e450c0336b358c1e56bbb0df909e19
Keesiu Wong <keesiu.wong@gmail.com>
2018-09-05 18:30:25 +0200

    add counter for logging

--- src/data/download.py
+++ src/data/download.py
@@ -13,13 +13,18 @@ def main(team_csv_path="data/raw/meta-kaggle-2016/Teams.csv", output_path="data/
     
     # load Teams.csv
     team_csv = pd.read_csv(team_csv_path, low_memory=False)
-    logger.info("Loaded Team.csv with {} entries.".format(len(team_csv)))
+    n_total = len(team_csv)
+    logger.info("Loaded Team.csv with {} entries.".format(n_total))
     
     # drop all entries, that don't have a repo entry
     team_csv = team_csv.dropna(subset=['GithubRepoLink'])
-    logger.info("In Team.csv {} entries have a GithubRepoLink.".format(len(team_csv)))
+    n_link = len(team_csv)
+    logger.info("In Team.csv from {} entries {} have a GithubRepoLink.".format(n_total, n_link))
     
     i = 0
+    n_try = 0
+    n_success = 0
+    n_error = 0
     start = time()
     for _, row in team_csv.iterrows():
         
@@ -34,6 +39,7 @@ def main(team_csv_path="data/raw/meta-kaggle-2016/Teams.csv", output_path="data/
         repo_url = row['GithubRepoLink']
         repo_zip_url = repo_url + "/archive/master.zip"
         try:
+            n_try += 1
             req = requests.get(repo_zip_url, allow_redirects=True)
             req.raise_for_status()
             z = zipfile.ZipFile(io.BytesIO(req.content))
@@ -41,14 +47,16 @@ def main(team_csv_path="data/raw/meta-kaggle-2016/Teams.csv", output_path="data/
             logger.info("---- Download of repo {:6} {:11} Team: {:20.15}"
                         .format(row['Id'], 'successful.', row['TeamName']+'.'))
             i += 1
+            n_success += 1
         except (requests.exceptions.RequestException,
                 requests.exceptions.HTTPError) as e:
             logger.error("---- Download of repo {:6} {:11} Team: {:20.15} {}"
                         .format(row['Id'], 'failed.', row['TeamName']+'.', e))
+            n_error += 1
         if i == 5: break
     end = time()
-    logger.info("Time needed for downloading repos: {}"
-                .format(pd.Timedelta(seconds=end-start).round(freq='s')))
+    logger.info("Successfully downloaded {} repos from {} tries ({} errors). Time needed: {}"
+                .format(n_success, n_try, n_error, pd.Timedelta(seconds=end-start).round(freq='s')))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
--- src/runall.py
+++ src/runall.py
@@ -1,4 +1,4 @@
 from src.data import download
 
-# download Github repositories from Team.csv to data/external/repositories
+#%% download Github repositories from Team.csv to data/external/repositories
 download.main()

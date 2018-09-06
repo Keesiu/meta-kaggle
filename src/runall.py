13ed851aa2b60727ac24fce065df15ee6481069b
Keesiu Wong <keesiu.wong@gmail.com>
2018-09-06 16:10:09 +0200

    uncommented download

--- src/runall.py
+++ src/runall.py
@@ -1,12 +1,12 @@
 import sys
 
 # forces import statement to also search in cwd (should be .../meta-kaggle)
-print(sys.path)
+# see: chrisyeh96.github.io/2017/08/08/definitive-guide-python-imports.html#more-on-syspath
 if '' not in sys.path:
     sys.path.insert(0, '')
-print(sys.path)
 
 from src.data import download
 
 #%% download Github repositories from Team.csv to data/external/repositories
-# download.main()
+
+ download.main()

b1b8c0a58d6e6dc6a1c022e872bd5e918b938dd2
Keesiu Wong <keesiu.wong@gmail.com>
2018-09-06 16:06:39 +0200

    fix sys.path for import

--- src/runall.py
+++ src/runall.py
@@ -1,4 +1,12 @@
+import sys
+
+# forces import statement to also search in cwd (should be .../meta-kaggle)
+print(sys.path)
+if '' not in sys.path:
+    sys.path.insert(0, '')
+print(sys.path)
+
 from src.data import download
 
 #%% download Github repositories from Team.csv to data/external/repositories
-download.main()
+# download.main()

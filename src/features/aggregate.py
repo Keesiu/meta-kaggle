f6e238aa7efc32c54cd663e882bc567d082c0d3d
Keesiu Wong <keesiu.wong@gmail.com>
2018-09-17 14:44:46 +0200

    created repos_df

--- src/features/aggregate.py
+++ src/features/aggregate.py
@@ -29,6 +29,12 @@ def main(metadata_path = "data/raw/meta-kaggle-2016",
     # load features_df
     features_df = pd.read_pickle(os.path.join(interim_path, 'features_df.pkl'))
     logger.info("Loaded Team.csv with {} files.".format(len(features_df)))
+    
+    # create repos_df by filtering for repos with features
+    ids_set = set(features_df['repo_id'])
+    in_features = [repo_id in ids_set for repo_id in teams_df['Id'].tolist()]
+    repos_df = teams_df[in_features].set_index('Id')
+    logger.info("Created repos_df.")
 
 
 if __name__ == '__main__':

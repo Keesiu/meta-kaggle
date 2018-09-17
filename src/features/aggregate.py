d94e9a16a9d4fd284523c0c7b70e96b7aef87a68
Keesiu Wong <keesiu.wong@gmail.com>
2018-09-17 17:11:15 +0200

    add aggregation functionality

--- src/features/aggregate.py
+++ src/features/aggregate.py
@@ -2,39 +2,91 @@
 
 import os, logging, argparse
 import pandas as pd
-import numpy as np
+import collections
+from time import time
 
 
 def main(metadata_path = "data/raw/meta-kaggle-2016",
          interim_path = "data/interim",
          processed_path = "data/processed"):
     
-    """Aggregates extracted features with Teams.csv to repos_df."""
+    """Aggregates extracted features to repos_df.
+    
+    Builds repos_df from Teams.csv by dropping all repos without python files,
+    aggregates all source code metrics by repository ID and compute basic
+    statistical metrics per repository, like mean and sum. Finally, combines
+    these statistics with repos_df and save it to <processed_path>."""
     
     # logging
     logger = logging.getLogger(__name__)
     
     # normalize paths
     metadata_path = os.path.normpath(metadata_path)
-    logger.debug("Path to meta-kaggle-2016 normalized: {}".format(metadata_path))
+    logger.debug("Path to meta-kaggle-2016 normalized: {}"
+                 .format(metadata_path))
     interim_path = os.path.normpath(interim_path)
-    logger.debug("Path to iterim data normalized: {}".format(interim_path))
+    logger.debug("Path to iterim data normalized: {}"
+                 .format(interim_path))
     processed_path = os.path.normpath(processed_path)
-    logger.debug("Path to processed data normalized: {}".format(processed_path))
+    logger.debug("Path to processed data normalized: {}"
+                 .format(processed_path))
     
     # load Teams.csv
-    teams_df = pd.read_csv(os.path.join(metadata_path, 'Teams.csv'), low_memory=False)
-    logger.info("Loaded Team.csv with {} repositories.".format(len(teams_df)))
+    teams_df = pd.read_csv(os.path.join(metadata_path, 'Teams.csv'),
+                           low_memory=False)
+    logger.info("Loaded Team.csv with {} repositories."
+                .format(len(teams_df)))
     
     # load features_df
     features_df = pd.read_pickle(os.path.join(interim_path, 'features_df.pkl'))
-    logger.info("Loaded Team.csv with {} files.".format(len(features_df)))
+    logger.info("Loaded features_df.pkl with {} files."
+                .format(len(features_df)))
+    
+    # start aggregation
+    start = time()
     
     # create repos_df by filtering for repos with features
-    ids_set = set(features_df['repo_id'])
+    ids_counter = collections.Counter(features_df['repo_id'])
+    ids_set = set(ids_counter)
     in_features = [repo_id in ids_set for repo_id in teams_df['Id'].tolist()]
     repos_df = teams_df[in_features].set_index('Id')
     logger.info("Created repos_df.")
+    
+    # add column 'n_scripts' which counts the number of scripts
+    repos_df['n_scripts'] = [ids_counter[key] for key in repos_df.index]
+    logger.debug("Added column 'n_scripts' which is the number of scripts.")
+    
+    # transform boolean columns (like _is_error flags) to integer
+    n_bool = len(features_df.select_dtypes('bool').columns)
+    for col in features_df.select_dtypes('bool').columns:
+        features_df[col] = features_df[col].astype(int)
+    logger.debug("Trasformed {} boolean columns to integer."
+                 .format(n_bool))
+    
+    # splits features_df along 'repo_id'
+    grouped = features_df.groupby('repo_id')
+    logger.debug("Splitted features_df into groups along repo_id.")
+    
+    # aggregates features
+    aggregated_df = grouped.describe()
+    logger.info("{} features aggregated to {} statistical features (max = {})"
+            .format(features_df.shape[1]-1,
+                    aggregated_df.shape[1],
+                    8*(features_df.shape[1]-1)))
+    
+    # combines aggregates features with repos_df
+    repos_df = pd.concat([repos_df, aggregated_df], axis=1)
+    logger.info("Combined aggregated features with repos_df.")
+    
+    # export repos_df as pickle file to processed folder
+    repos_df.to_pickle(os.path.join(processed_path, 'repos_df.pkl'))
+    logger.info("Saved repos_df to {}."
+            .format(os.path.join(processed_path, 'repos_df.pkl')))
+    
+    # logging time passed
+    end = time()
+    time_passed = pd.Timedelta(seconds=end-start).round(freq='s')
+    logger.info("Time needed to extract the features: {}".format(time_passed))
 
 
 if __name__ == '__main__':
@@ -43,25 +95,28 @@ if __name__ == '__main__':
     logging.basicConfig(
         level = logging.DEBUG,
         format = "%(asctime)s %(name)-20s %(levelname)-8s %(message)s",
-        filename = "logs/extract.log",
+        filename = "logs/aggregate.log",
         datefmt = "%a, %d %b %Y %H:%M:%S")
     
     # parse arguments
     parser = argparse.ArgumentParser(
-            description = "Aggregates extracted features with Teams.csv to repos_df.")
+            description = "Aggregates extracted features to repos_df.")
     parser.add_argument(
             '-m', '--metadata_path',
             default = "data/raw/meta-kaggle-2016",
-            help = "path to Kaggle Meta Dataset 2016, where Teams.csv is (default: data/raw/meta-kaggle-2016)")
+            help = """path to Kaggle Meta Dataset 2016, where Teams.csv is
+                    (default: data/raw/meta-kaggle-2016)""")
     parser.add_argument(
             '-i', '--interim_path',
             default = "data/interim",
-            help = "path to extracted features features_df.pkl (default: data/interim)")
+            help = """path to extracted features features_df.pkl
+                    (default: data/interim)""")
     parser.add_argument(
             '-p', '--processed_path',
             default = "data/processed",
-            help = "path to store the aggregated output repos_df.pkl (default: data/processed)")
+            help = """path to store the aggregated output repos_df.pkl
+                    (default: data/processed)""")
     args = parser.parse_args()
     
     # run main
-    main(args.interim_path)
+    main(args.metadata_path, args.interim_path, args.processed_path)

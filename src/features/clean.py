0e338fd1f80dcfa98e8ae35cf6b93552030c611f
Keesiu Wong <keesiu.wong@gmail.com>
2018-09-19 11:57:07 +0200

    setup basic environment

--- src/features/clean.py
+++ src/features/clean.py
@@ -2,15 +2,40 @@
 
 import os, logging, argparse
 import pandas as pd
-import collections
 from time import time
 
 
-def main():
+def main(interim_path = "data/interim",
+         processed_path = "data/processed"):
     
     """Cleans aggregated data."""
     
+    # logging
+    logger = logging.getLogger(__name__)
     
+    # normalize paths
+    interim_path = os.path.normpath(interim_path)
+    logger.debug("Path to iterim data normalized: {}"
+                 .format(interim_path))
+    processed_path = os.path.normpath(processed_path)
+    logger.debug("Path to processed data normalized: {}"
+                 .format(processed_path))
+    
+    # load aggregated_df
+    aggregated_df = pd.read_pickle(os.path.join(interim_path, 'aggregated_df.pkl'))
+    logger.info("Loaded aggregated_df.pkl with {} files."
+                .format(len(aggregated_df)))
+    
+    # start cleaning
+    start = time()
+    
+    # correlation coefficient matrix
+    # clean data
+    
+    # logging time passed
+    end = time()
+    time_passed = pd.Timedelta(seconds=end-start).round(freq='s')
+    logger.info("Time needed to extract the features: {}".format(time_passed))
 
 if __name__ == '__main__':
     
@@ -18,5 +43,23 @@ if __name__ == '__main__':
     logging.basicConfig(
         level = logging.DEBUG,
         format = "%(asctime)s %(name)-20s %(levelname)-8s %(message)s",
-        filename = "logs/aggregate.log",
+        filename = "logs/clean.log",
         datefmt = "%a, %d %b %Y %H:%M:%S")
+    
+    # parse arguments
+    parser = argparse.ArgumentParser(
+            description = "Cleans aggregated data.")
+    parser.add_argument(
+            '-i', '--interim_path',
+            default = "data/interim",
+            help = """path to extracted features features_df.pkl
+                    (default: data/interim)""")
+    parser.add_argument(
+            '-p', '--processed_path',
+            default = "data/processed",
+            help = """path to store the aggregated output repos_df.pkl
+                    (default: data/processed)""")
+    args = parser.parse_args()
+    
+    # run main
+    main(args.interim_path, args.processed_path)

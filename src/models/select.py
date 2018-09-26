5acb1de5199e005d1baa90c7549b0096f7006ed8
Keesiu Wong <keesiu.wong@gmail.com>
2018-09-26 15:15:32 +0200

    add X,y split and improved logging

--- src/models/select.py
+++ src/models/select.py
@@ -2,6 +2,7 @@
 
 import os, logging, argparse
 import pandas as pd
+import numpy as np
 from time import time
 
 
@@ -22,30 +23,44 @@ def main(processed_path = "data/processed"):
     logger.info("Loaded df.pkl. Shape of df: {}"
                 .format(df.shape))
     
+    # split df into dependent and independent variables
+    y, X = np.split(df, [2], axis=1)
+    
     #%% start feature selection
     start = time()
     
-    n = len(df)
-    
-    selected_df = df.copy()
+    n = len(X)
     
     # drop features with more than 95% zeros
-    selected_df.drop(columns = df.columns[(((df == 0).sum()/n) > .95).values],
-                     errors = 'ignore',
-                     inplace = True)
+    col_old = set(X.columns)
+    dropped = df.columns[(((df == 0).sum()/n) > .95).values].tolist()
+    X.drop(columns=dropped, errors='ignore', inplace=True)
+    col_new = set(X.columns)
+    logger.info("{} features have more than 95% zeros. Dropped {}."
+                .format(len(dropped), len(col_old)-len(col_new)))
+    for drop in col_old-col_new:
+        logger.debug("Dropped: {}".format(drop))
     
     # drop features with less than 5% unique values
-    selected_df.drop(columns = df.columns[(df.nunique()/n < .05).values],
-                     errors = 'ignore',
-                     inplace = True)
+    col_old = set(X.columns)
+    dropped = df.columns[(df.nunique()/n < .05).values]
+    X.drop(columns=dropped, errors='ignore', inplace=True)
+    col_new = set(X.columns)
+    logger.info("{} features have less than 5% unique values. Dropped {}."
+                .format(len(dropped), len(col_old)-len(col_new)))
+    for drop in col_old-col_new:
+        logger.debug("Dropped: {}".format(drop))
     
         
     # drop features with too little relative standard deviation
+    list(X.std()/X.mean())
+    
     
     # remove multi-collinearity
     
     #%% export selected_df as pickle file to processed folder
-    df.to_pickle(os.path.join(processed_path, 'selected_df.pkl'))
+    selected_df = pd.concat([y, X], axis=1)
+    selected_df.to_pickle(os.path.join(processed_path, 'selected_df.pkl'))
     logger.info("Saved selected_df to {}."
             .format(os.path.join(processed_path, 'selected_df.pkl')))
     

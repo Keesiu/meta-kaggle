d184a53d81ddcdfb119cc5614c3587d5100b5ada
Keesiu Wong <keesiu.wong@gmail.com>
2018-09-30 23:14:58 +0200

    also deletes interim and processed data

--- src/reset.py
+++ src/reset.py
@@ -3,7 +3,10 @@
 import os, shutil, argparse
 
 
-def main(repos_path = "data/external/repositories", logs_path = "logs"):
+def main(repos_path = "data/external/repositories",
+         interim_path = "data/interim",
+         processed_path = "data/processed",
+         logs_path = "logs"):
     
     """Resets project for clean re-run.
     
@@ -18,11 +21,17 @@ def main(repos_path = "data/external/repositories", logs_path = "logs"):
     if os.path.exists(repos_path):
         shutil.rmtree(repos_path)
     
-    # delete all interim data
+    # delete all pickle files in interim data
+    for file in os.listdir(interim_path):
+        if file[-4:] == '.pkl':
+            os.remove(os.path.join(interim_path, file))
     
-    # delete all processed data
+    # delete all pickle files in processed data
+    for file in os.listdir(processed_path):
+        if file[-4:] == '.pkl':
+            os.remove(os.path.join(processed_path, file))
     
-    # delete all log-files
+    # delete all log-files in logs
     for file in os.listdir(logs_path):
         if file[-4:] == '.log':
             os.remove(os.path.join(logs_path, file))
@@ -30,17 +39,32 @@ def main(repos_path = "data/external/repositories", logs_path = "logs"):
 
 if __name__ == '__main__':
     
+    # parse arguments
     parser = argparse.ArgumentParser(
             description = "Resets project for clean re-run.")
     parser.add_argument(
-            '-r', '--repo_path',
+            '--repos_path',
             default = "data/external/repositories",
             help = "path to downloaded repositories \
                     (default: data/external/repositories)")
     parser.add_argument(
-            '-l', '--logs_path',
+            '--interim_path',
+            default = "data/interim",
+            help = "path to interim data: tabled_df, extracted_df, aggregated_df \
+                    (default: data/interim)")
+    parser.add_argument(
+            '--processed_path',
+            default = "data/processed",
+            help = "path to processed data: cleaned_df, selected_df \
+                    (default: data/processed)")
+    parser.add_argument(
+            '--logs_path',
             default = "logs",
-            help = "path to log-files (default: logs)")
+            help = "path to log files (default: logs)")
     args = parser.parse_args()
     
-    main(args.repo_path, args.logs_path)
+    # run main
+    main(args.repos_path,
+         args.interim_path,
+         args.processed_path,
+         args.logs_path)

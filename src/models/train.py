77f6d2edab36a784942f42f21bdb4c4dafddabc5
Keesiu Wong <keesiu.wong@gmail.com>
2018-10-14 23:03:11 +0200

    change LassoCV to ElasticNetCV, normalization, df_name, y_name, pickle output

--- src/models/train.py
+++ src/models/train.py
@@ -4,16 +4,18 @@ import os, logging, argparse
 import pandas as pd
 import numpy as np
 from time import time
-from sklearn.linear_model import LinearRegression, LassoCV, Lasso
+from sklearn.linear_model import LinearRegression, LassoCV, Lasso, ElasticNetCV, ElasticNet
 from sklearn.model_selection import train_test_split, cross_val_score, cross_validate
 import statsmodels.api as sm
 from statsmodels.formula.api import ols
 import matplotlib.pyplot as plt
+import pickle
 
 
 def main(processed_path = "data/processed",
          models_path = "models",
-         df_name = 'cleaned'):
+         df_name = 'cleaned',
+         y_name = 'ranking_log'):
     
     """Trains the model."""
     
@@ -30,18 +32,21 @@ def main(processed_path = "data/processed",
     
     # load df, either cleaned, cleaned_pca, selected, selected_pca
     df = pd.read_pickle(os.path.join(processed_path, df_name+'_df.pkl'))
-    logger.info("Loaded {}. Shape of df: {}"
+    logger.info("Loaded '{}'. Shape of df: {}"
                 .format(df_name+'_df.pkl', df.shape))
+    # wheather PCA was performed
+    PCA = True if df_name[-4:] == '_pca' else False
     
     # split df into dependent and independent variables
     y, X = np.split(df, [2], axis=1)
     X_columns = X.columns
     X_index = X.index
-    y_s = y.score
-    y_r = y.ranking_log
+    X = X.values
+    # set y to either ranking_log or score_neg_log
+    y = y[y_name].values
+    logger.info("Set y to '{}'.".format(y_name))
     
-    #%% start training
-    
+    #%% other stuff
     
 #    # train-test-split
 #    X_train, X_test, y_train, y_test  = train_test_split(
@@ -81,82 +86,129 @@ def main(processed_path = "data/processed",
     
     start = time()
     
-    # print R^2 values for bounding alphas 0 and 1 to make sense of alphas
-    logger.info("R^2 for alpha = 0: {}"
-                .format(Lasso(alpha=0, random_state=42)
-                        .fit(X.values, y_r.values)
-                        .score(X.values, y_r.values)))
-    logger.info("R^2 for alpha = 1: {}"
-                .format(Lasso(alpha=1, random_state=42)
-                        .fit(X.values, y_r.values)
-                        .score(X.values, y_r.values)))
+    #%% define hyperparameter
     
-    # define list of 100 alphas to test: from 1 logarithmically decreasing to 0
-    BASE = 1 + 1/5
-    logger.debug("Constant BASE is set to {}.".format(BASE))
-    alphas = [BASE**(-x) for x in range(100)]
+#    # define list of 100 alphas to test: from 1 logarithmically decreasing to 0
+#    BASE = 1 + 1/5
+#    logger.debug("Constant BASE is set to {}.".format(BASE))
+#    ALPHAS = [BASE**(-x) for x in range(100)]
+    ALPHAS = [.00001, .0001, .001, .01, .1, .25, .5, .75, .95, .99999]
+    logger.debug("Alphas set to {}".format(ALPHAS))
     
-    # define hyperparameter
-    CV = 20
+    # define list of l1-ratios to test
+    # Note that a good choice of list of values for l1_ratio is often to put
+    # more values close to 1 (i.e. Lasso) and less close to 0 (i.e. Ridge),
+    # as in [.1, .5, .7, .9, .95, .99, 1]
+    L1_RATIOS = [1-a for a in ALPHAS]
+    logger.debug("L1_ratios set to {}".format(L1_RATIOS))
+    
+    # define other hyperparameter
+    CV = 10
     RS = 42
     N_JOBS = -1
     SELECTION = 'random'
+    # normalize data only if PCA was not performed (because PCA standardized)
+    # If True, the regressors X will be normalized before regression
+    # by subtracting the mean and dividing by the l2-norm.
+    NORMALIZE = not PCA
+    logger.debug("CV={}, RS={}, N_JOBS={}, SELECTION={}, NORMALIZE={}"
+                 .format(CV, RS, N_JOBS, SELECTION, NORMALIZE))
     
-    # train LassoCV
-    t1 = time()
-    lasso = LassoCV(cv=CV, alphas=alphas,
-                    random_state=RS, selection=SELECTION, n_jobs= N_JOBS) \
-            .fit(X.values, y_r.values)
-    t2 = time()
-    print("Time to perform cross-validation: {}"
-          .format(pd.Timedelta(seconds=t2-t1).round(freq='s')))
+    #%% print R^2 values for bounding alphas 0 and 1 to make sense of alphas
+    logger.info("R^2 for alpha=0: {}"
+                .format(ElasticNet(alpha=0, l1_ratio=.5, normalize=NORMALIZE, random_state=42)
+                        .fit(X, y)
+                        .score(X, y)))
+    logger.info("R^2 for alpha=1: {}"
+                .format(ElasticNet(alpha=1, l1_ratio=.5, normalize=NORMALIZE, random_state=42)
+                        .fit(X, y)
+                        .score(X, y)))
+    
+    #%% train model
+    mod = ElasticNetCV(cv=CV, alphas=ALPHAS, l1_ratio=L1_RATIOS, normalize=NORMALIZE,
+                       random_state=RS, selection=SELECTION, n_jobs= N_JOBS) \
+          .fit(X, y)
     
     # log some statistics
-    logger.info("Lasso score = {}.".format(lasso.score(X.values, y_r.values)))
-    lasso_alpha = lasso.alpha_
-    logger.info("Lasso alpha = {}.".format(lasso_alpha))
-    lasso_coef = pd.Series(data=lasso.coef_, index=X_columns)
-    logger.info("Lasso coefficients = {}.".format(lasso_coef))
-    lasso_mse_path = pd.DataFrame(data=lasso.mse_path_, index=alphas)
-    logger.info("Lasso MSE = {}.".format(lasso_mse_path))
+    logger.info("best R^2 score: {}".format(mod.score(X, y)))
+    l1_ratio = mod.l1_ratio_
+    logger.info("best l1_ratio: {}".format(l1_ratio))
+    alpha = mod.alpha_
+    logger.info("best alpha: {}".format(alpha))
+    coef = pd.Series(data=mod.coef_, index=X_columns)
+    logger.debug("best coefficients:\n{}".format(coef))
     
-    # plot all folds with all
-    
-    
-    m_log_alphas = -np.log(lasso.alphas_)/np.log(BASE)
-    # Display results
-    plt.figure(figsize=(10,8))
-    ymin, ymax = 0, 1000
-    plt.plot(m_log_alphas, lasso.mse_path_, ':')
-    plt.plot(m_log_alphas, lasso.mse_path_.mean(axis=-1), 'k',
-             label='Average across the folds', linewidth=2)
-    plt.axvline(-np.log10(lasso.alpha_), linestyle='--', color='k',
-                label='alpha: CV estimate')
-    
-    plt.legend()
-    
-    plt.xlabel('-log(alpha)')
-    plt.ylabel('Mean square error')
-    plt.title('Mean square error on each fold')
-    plt.axis('tight')
-    plt.ylim(ymin, ymax)
-    plt.show()
+#    mse_path = pd.DataFrame(data=mod.mse_path_, index=ALPHAS)
+#    logger.info("Lasso MSE = {}.".format(mse_path))
+#    # Display results
+#    m_log_alphas = -np.log(mod.alphas_)/np.log(BASE)
+#    plt.figure(figsize=(10,8))
+#    ymin, ymax = 0, 1000
+#    plt.plot(m_log_alphas, mod.mse_path_, ':')
+#    plt.plot(m_log_alphas, mod.mse_path_.mean(axis=-1), 'k',
+#             label='Average across the folds', linewidth=2)
+#    plt.axvline(-np.log10(mod.alpha_), linestyle='--', color='k',
+#                label='alpha: CV estimate')
+#    plt.legend()
+#    plt.xlabel('-log(alpha)')
+#    plt.ylabel('Mean square error')
+#    plt.title('Mean square error on each fold')
+#    plt.axis('tight')
+#    plt.ylim(ymin, ymax)
+#    plt.show()
     
     # Nested Cross-Validation
-    lasso = LassoCV(cv=CV, alphas=alphas,
-                    random_state=RS, selection=SELECTION, n_jobs= N_JOBS)
-    t1 = time()
-    cv_results = cross_validate(lasso, X.values, y_r.values,
-                                return_train_score=True, cv=10, n_jobs=N_JOBS)
-    t2 = time()
-    print("Time to perform nested cross-validation: {}"
-          .format(pd.Timedelta(seconds=t2-t1).round(freq='s')))
-    print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+    cv_results = cross_validate(ElasticNetCV(cv=CV,
+                                             alphas=ALPHAS,
+                                             normalize=NORMALIZE,
+                                             random_state=RS,
+                                             selection=SELECTION,
+                                             n_jobs= N_JOBS),
+                                X, y, cv=10,
+                                return_train_score=True, n_jobs=N_JOBS)
+    logger.info("95% confidence intervall: {:05.2f} +/-{:05.2f} (mean +/- 2 std)"
+                .format(cv_results['test_score'].mean(),
+                        cv_results['test_score'].std()*2))
+    logger.debug("Nested cross-validation results:\n{}"
+                .format(pd.DataFrame(data=cv_results)))
     
-    # Linear regression with lasso regularization
-    mod = sm.OLS(y_r, sm.add_constant(X))
-    res = mod.fit_regularized(method='elastic_net', alpha=lasso_alpha, L1_wt=1.0, refit=True)
-    print(res.summary())
+    # Elastic Net regression with statsmodels
+    mod2 = sm.OLS(y, sm.add_constant(pd.DataFrame(data=X,
+                                                  columns=X_columns,
+                                                  index=X_index)))\
+          .fit_regularized(method='elastic_net',
+                           alpha=alpha,
+                           L1_wt=l1_ratio,
+                           refit=True)
+    res = mod2.summary().as_text()
+    logger.info("ElasticNet regression (from sm) of '{}' with respect to '{}' with alpha={} and L1_wt={}:\n{}"
+                .format(df_name, y_name, alpha, l1_ratio, res))
+    
+    #%% export results as pickle file to models folder
+    
+    # pickle mod
+    with open(os.path.join(models_path, df_name+'_'+y_name+'_sklearn_ElasticNetCV.pkl'), 'wb') as handle:
+        pickle.dump(mod, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    logger.info("Saved elastic net model of sklearn to {}."
+                .format(os.path.join(models_path, df_name+'_'+y_name+'_sklearn_ElasticNetCV.pkl')))
+    
+    # pickle mod2
+    with open(os.path.join(models_path, df_name+'_'+y_name+'_sm_OLS_fit_regularized.pkl'), 'wb') as handle:
+        pickle.dump(mod2, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    logger.info("Saved elastic net model of statsmodels to {}."
+                .format(os.path.join(models_path, df_name+'_'+y_name+'_sm_OLS_fit_regularized.pkl')))
+    
+    # save res as .txt
+    f = open(os.path.join(models_path, df_name+'_'+y_name+'_sm_OLS_fit_regularized_summary.txt'), "w+")
+    f.write(res)
+    f.close()
+    
+    
+    #%% logging time passed
+    end = time()
+    time_passed = pd.Timedelta(seconds=end-start).round(freq='s')
+    logger.info("Time needed to train Elastic Net Model on '{}' with respect to '{}': {}"
+                .format(df_name+'_df.pkl', y_name, time_passed))
     
 #%%
 if __name__ == '__main__':
@@ -181,7 +233,19 @@ if __name__ == '__main__':
             default = "models",
             help = "path to save the trained models \
                     (default: models)")
+    parser.add_argument(
+            '--df_name',
+            default = 'cleaned',
+            help = "name of df to be trained on \
+                    either cleaned, cleaned_pca, selected, selected_pca \
+                    (default: cleaned)")
+    parser.add_argument(
+            '--y_name',
+            default = 'ranking_log',
+            help = "name of dependent variable to be trained on \
+                    either 'ranking_log' or 'score_neg_log' \
+                    (default: ranking_log)")
     args = parser.parse_args()
     
     # run main
-    main(args.processed_path, args.models_path)
+    main(args.processed_path, args.models_path, args.df_name, args.y_name)
